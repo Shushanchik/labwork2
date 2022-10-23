6b4065e713ab6de88d7b6b1f0b0df4aa337ed088
Shushanchik <113179077+Shushanchik@users.noreply.github.com>
2022-10-24 00:02:50 +0300

    Deadline_3

--- lib/number.h
+++ lib/number.h
@@ -1,10 +1,11 @@
 #pragma once
 #include <cinttypes>
 #include <iostream>
-
-
+#include <cmath>
+const int uint2022_size = 68;
+const int radix = 1000000000;
 struct uint2022_t {
-    // implement
+    unsigned int digits[uint2022_size] = {0 };
 };
 
 static_assert(sizeof(uint2022_t) <= 300, "Size of uint2022_t must be no higher than 300 bytes");
@@ -17,12 +18,23 @@ uint2022_t operator+(const uint2022_t& lhs, const uint2022_t& rhs);
 
 uint2022_t operator-(const uint2022_t& lhs, const uint2022_t& rhs);
 
-uint2022_t operator*(const uint2022_t& lhs, const uint2022_t& rhs);
+uint2022_t operator * (const uint2022_t& lhs, const uint2022_t& rhs);
+
+uint2022_t operator * (uint32_t lhs_int, const uint2022_t& rhs);
+
+uint2022_t operator * (const uint2022_t& lhs, uint32_t rhs_int);
 
 uint2022_t operator/(const uint2022_t& lhs, const uint2022_t& rhs);
 
-bool operator==(const uint2022_t& lhs, const uint2022_t& rhs);
+bool operator < (const uint2022_t lhs, const uint2022_t rhs);
 
-bool operator!=(const uint2022_t& lhs, const uint2022_t& rhs);
+bool operator <= (const uint2022_t lhs, const uint2022_t rhs);
 
+bool operator == (const uint2022_t& lhs, const uint2022_t& rhs);
+
+bool operator != (const uint2022_t& lhs, const uint2022_t& rhs);
+
+bool operator >= (const uint2022_t lhs, const uint2022_t rhs);
+
+bool operator > (const uint2022_t lhs, const uint2022_t rhs);
 std::ostream& operator<<(std::ostream& stream, const uint2022_t& value);
